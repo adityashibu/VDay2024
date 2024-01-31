f499ee318b55442d0c45d04e15eaa1e4d498011a
Aditya Shibu <as2397@hw.ac.uk>
2024-01-31 09:25:46 +0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
-      document.querySelector('.glowingText').classList.add('typing-animation');
       clearTimeout(c);
     }, 1000);
   };
@@ -9,6 +8,4 @@ onload = () => {
 window.addEventListener('load', (event) => {
     document.body.classList.remove("not-loaded");
     document.querySelector('.glowingText').style.opacity = '1';
-
-    document.querySelector('.glowingText').classList.add('typing-animation');
 });
