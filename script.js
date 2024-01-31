3995df54f13949f91e930f8fcf3e0d6e3874f832
Aditya Shibu <as2397@hw.ac.uk>
2024-01-31 08:52:56 +0400

    Added animation to the text

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
+      document.querySelector('.glowingText').classList.add('typing-animation');
       clearTimeout(c);
     }, 1000);
   };
+
+window.addEventListener('load', (event) => {
+    document.body.classList.remove("not-loaded");
+    document.querySelector('.glowingText').style.opacity = '1';
+
+    document.querySelector('.glowingText').classList.add('typing-animation');
+});
